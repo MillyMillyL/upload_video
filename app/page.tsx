972a90fefd55aa93e0a5ae34375f48777b377276
Milly <liuhongmein@gmail.com>
2024-04-29 15:11:14 -0400

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,133 @@
-import Image from "next/image";
+"use client";
+import { z } from "zod";
+import axios from "axios";
+import { Progress } from "@/components/ui/progress";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { ChangeEvent, useState } from "react";
 
-export default function Home() {
+const fileSchema = z.object({
+  name: z.string(),
+  size: z.number(),
+  type: z.enum(["video/mp4", "image/jpeg"]),
+});
+
+const fileUploadSchema = z.object({
+  name: z.string(),
+  size: z.number(),
+  type: z.enum(["video/mp4", "image/jpeg"]),
+  fileName: z.string(),
+  uploadedSize: z.number(),
+  file: z.instanceof(Blob),
+});
+
+type FileUploadSchema = z.infer<typeof fileUploadSchema>;
+
+function createFormData({
+  name,
+  type,
+  size,
+  fileName,
+  uploadedSize,
+  file,
+}: FileUploadSchema) {
+  const fd = new FormData();
+  fd.append("name", name);
+  fd.append("type", type);
+  fd.append("size", size.toString());
+  fd.append("fileName", fileName);
+  fd.append("uploadedSize", uploadedSize.toString());
+  fd.append("file", file);
+
+  return fd;
+}
+
+function MainPage() {
+  const [file, setFile] = useState<File | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [processMax, setProcessMax] = useState<number>(100);
+  const [processValue, setProcessValue] = useState<number>(0);
+
+  function handleFileChange(e: ChangeEvent<HTMLInputElement>) {
+    const selectedFile = e.target.files && e.target.files[0];
+    console.log(selectedFile?.type);
+
+    if (selectedFile) {
+      const fileValidation = fileSchema.safeParse(selectedFile);
+
+      if (fileValidation.success) {
+        setFile(selectedFile);
+        setError(null);
+      } else {
+        setError("Invalid file format, only mp4 video is allowed");
+      }
+    }
+  }
+
+  let uploadedSize = 0;
+
+  async function handleUpload() {
+    if (!file) {
+      setError("Please select a mp4 video to upload");
+    } else if (file.type !== "video/mp4" && file.type !== "image/jpeg") {
+      setError("Only MP4 video and JPG image are supported");
+    } else {
+      const { name, size, type } = file;
+      const fileName = new Date().getTime() + "_" + name;
+
+      let uploadedResult = null;
+      setProcessMax(size);
+
+      while (uploadedSize < size) {
+        const fileChunk = file.slice(uploadedSize, uploadedSize + 64 * 1024);
+
+        const formData = createFormData({
+          name,
+          type,
+          size,
+          fileName,
+          uploadedSize,
+          file: fileChunk,
+        });
+
+        try {
+          uploadedResult = await axios.post(
+            "http://localhost:3000/api/upload",
+            formData
+          );
+
+          console.log(uploadedResult);
+        } catch (error) {
+          console.error("Error uploading file:", error);
+          alert("An error occurred while uploading the file");
+          return;
+        }
+
+        uploadedSize += fileChunk.size;
+        setProcessValue(uploadedSize);
+      }
+
+      alert("Uploaded successfully!");
+      setProcessValue(0);
+      setProcessMax(100);
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="container mx-auto w-full max-w-sm items-center space-y-8 mt-8">
+      <Progress value={33} max={processMax} />
+
+      <div className="grid w-full max-w-sm items-center gap-4">
+        <Label htmlFor="video">Upload Video</Label>
+        <Input id="video" type="file" onChange={handleFileChange} />
+        {error && <p className="text-destructive">{error}</p>}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Button variant="outline" size="lg" onClick={handleUpload}>
+        Upload Video
+      </Button>
+    </div>
   );
 }
+
+export default MainPage;
